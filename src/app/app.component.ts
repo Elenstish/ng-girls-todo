7828ff59b451313bca01272902353c61b6211a02
Elenstish <elenstish@gmail.com>
2020-12-01 21:15:36 +0200

    add list manager

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,5 @@ import {TodoItem} from './interface/todo-item';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo-list';
-  todoList: TodoItem[] = [
-    {title: 'install NodeJS'},
-    {title: 'install Angular CLI'},
-    {title: 'create new app'},
-    {title: 'serve app'},
-    {title: 'develop app'},
-    {title: 'deploy app'},
-  ];
-
-  addItem(title: string) {
-    this.todoList.push({ title });
-  }
+  title = 'My To Do List APP';
 }
